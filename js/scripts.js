f97a770742e4866300f325acbbe5093b362275e4
Sailor Winkelman <sailorwinkelman@gmail.com>
2016-10-24 16:41:07 -0700

    Add html and js for a second set of user responses.

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,11 @@ $(document).ready(function(){
       var worktransportationMode = $(this).val();
       $('#work-responses').append(worktransportationMode + "<br>");
     });
+    $("#fun-responses").show();
+    $("input:checkbox[name=fun-transportation]:checked").each(function(){
+      var funTransportationMode = $(this).val();
+      $('#fun-responses').append(funTransportationMode + "<br>");
+    })
     $('#transportation_survey').hide();
   });
 });
